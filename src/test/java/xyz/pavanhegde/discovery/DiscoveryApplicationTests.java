92bb3e7dc618ebc9c86461ead1e00abc1240bc57
Pavan Hegde <pavanrkadave@gmail.com>
2024-09-15 23:36:32 +0200

    Update discovery repo

---
+++ src/test/java/xyz/pavanhegde/discovery/DiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package xyz.pavanhegde.discovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
